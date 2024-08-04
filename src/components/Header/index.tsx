70aad19a7d15038738fcfb3d9b84b2f58dd32b16
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-08-04 21:22:51 +0700

    feat(Header): добавил шапку

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+import styles from "./index.module.scss";
+
+export const Header = () => {
+	return (
+		<header className={styles.header__wrapper}>
+			<div className="container">
+				<nav className={styles.header__nav}>
+					<ul className={styles.header__list}>
+						<li className={styles.header__item}>
+							<Link className={styles.header__link} href="#">
+								Local storage
+							</Link>
+						</li>
+						<li className={styles.header__item}>
+							<Link className={styles.header__link} href="#">
+								Query parameters
+							</Link>
+						</li>
+						<li className={styles.header__item}>
+							<Link className={styles.header__link} href="#">
+								API
+							</Link>
+						</li>
+						<li className={styles.header__item}>
+							<Link className={styles.header__link} href="#">
+								Forms
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
+};
