373679c53948a16c06af87efc7bbeb27a73913fa
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-08-05 03:22:04 +0700

    feat(forms): добавил страницу

---
+++ src/views/forms/types.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const FormsSchema = z.object({
+	email: z
+		.string()
+		.min(1, { message: "Поле обязательно для заполнения" })
+		.email("Валидация Email не пройдена"),
+	name: z.string().min(1, { message: "Поле обязательно для заполнения" }),
+	age: z
+		.number({ message: "Поле должно быть числом" })
+		.min(1, { message: "Поле обязательно для заполнения" }),
+});
+
+export type TForms = z.infer<typeof FormsSchema>;
--- src/views/forms/ui/FormsPage.tsx
+++ src/views/forms/ui/FormsPage.tsx
@@ -1,22 +1,65 @@
+"use client";
+
+import { ZodError } from "zod";
+import { FormEventHandler, useState } from "react";
+
 import { InputForm } from "@/shared/ui/inputForm";
 import { Button } from "@/shared/ui/button";
+
+import { TForms, FormsSchema } from "../types";
 
 import styles from "./index.module.scss";
 
 export const FormsPage = () => {
+	const [zodError, setZodError] = useState<ZodError | null>(null);
+	const [email, setEmail] = useState<string>("");
+	const [name, setName] = useState<string>("");
+	const [age, setAge] = useState<number>(0);
+
+	const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+		const data: TForms = { email, name, age };
+		const { success, error } = FormsSchema.safeParse(data);
+
+		if (error) {
+			setZodError(error);
+		}
+
+		if (success) {
+			setZodError(null);
+		}
+	};
+
 	return (
 		<div className="container">
 			<div className={styles.forms__wrapper}>
-				<form className={styles.forms__form}>
+				<form className={styles.forms__form} onSubmit={onSubmit}>
 					<div className={styles.forms__inputs}>
-						<InputForm labelProps={{ children: "Email" }} inputProps={{}} />
-						<InputForm labelProps={{ children: "Имя" }} />
-						<InputForm labelProps={{ children: "Возраст" }} />
+						<InputForm
+							labelProps={{ children: "Email" }}
+							inputProps={{ onChange: (e) => setEmail(e.target.value) }}
+						/>
+						<InputForm
+							labelProps={{ children: "Имя" }}
+							inputProps={{ onChange: (e) => setName(e.target.value) }}
+						/>
+						<InputForm
+							labelProps={{ children: "Возраст" }}
+							inputProps={{ onChange: (e) => setAge(+e.target.value) }}
+						/>
 					</div>
 
 					<Button style={{ width: "100%" }}>Отправить</Button>
 				</form>
 			</div>
+
+			{zodError && (
+				<div className={styles.forms__error}>
+					<p className={styles.forms__errorText}>
+						{JSON.stringify(zodError, null, " ")}
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
